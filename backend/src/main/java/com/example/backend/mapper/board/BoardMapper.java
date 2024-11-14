3633079bbd6de53d58437111d3f1ed6388cc35e0
June <junetiger10@naver.com>
2024-11-14 14:17:42 +0900

    한 게시물 보기

--- backend/src/main/java/com/example/backend/mapper/board/BoardMapper.java
+++ backend/src/main/java/com/example/backend/mapper/board/BoardMapper.java
@@ -24,4 +24,11 @@ public interface BoardMapper {
             ORDER BY id DESC
             """)
     List<Board> selectAll();
+
+    @Select("""
+            SELECT *
+            FROM board
+            WHERE id = #{id};
+            """)
+    Board selectById(int id);
 }
