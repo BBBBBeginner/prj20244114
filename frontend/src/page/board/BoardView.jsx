6006349c9ddfa99fd53f20fa2b8f15e7fe8471c3
June <junetiger10@naver.com>
2024-11-14 16:26:25 +0900

    삭제 후 토스트 출력, 목록으로 이동

--- frontend/src/page/board/BoardView.jsx
+++ frontend/src/page/board/BoardView.jsx
@@ -1,12 +1,15 @@
-import { useParams } from "react-router-dom";
-import { Box, Button, Input, Spinner, Stack, Textarea } from "@chakra-ui/react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Box, Input, Spinner, Stack, Textarea } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Field } from "../../components/ui/field.jsx";
+import { Button } from "../../components/ui/button.jsx";
+import { toaster } from "../../components/ui/toaster.jsx";
 
 export function BoardView() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get(`/api/board/view/${id}`).then((res) => setBoard(res.data));
@@ -17,8 +20,25 @@ export function BoardView() {
   }
 
   const handleDeleteClick = () => {
-    axios.delete(`/api/board/delete/${board.id}`);
+    axios
+      .delete(`/api/board/delete/${board.id}`)
+      .then((res) => res.data)
+      .then((data) => {
+        toaster.create({
+          type: data.message.type,
+          description: data.message.text,
+        });
+        navigate("/");
+      })
+      .catch((e) => {
+        const data = e.response.data;
+        toaster.create({
+          type: data.message.type,
+          description: data.message.text,
+        });
+      });
   };
+
   return (
     <Box>
       <h3>{id} 번 게시물</h3>
