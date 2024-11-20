1b50b953f5cef32571ac05b9eecd1ea5b81d7a7e
June <junetiger10@naver.com>
2024-11-20 11:10:01 +0900

    댓글 목록 보기

--- frontend/src/components/comment/CommentContainer.jsx
+++ frontend/src/components/comment/CommentContainer.jsx
@@ -10,7 +10,7 @@ export function CommentContainer({ boardId }) {
       <Stack gap={5}>
         <h3>댓글</h3>
         <CommentInput boardId={boardId} />
-        <CommentList />
+        <CommentList boardId={boardId} />
       </Stack>
     </Box>
   );
