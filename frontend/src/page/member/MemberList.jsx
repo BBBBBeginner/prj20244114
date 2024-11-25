d97e4839cb0779775d1aa5dfa70edea40063023a
June <junetiger10@naver.com>
2024-11-25 11:01:37 +0900

    회원목록

--- frontend/src/page/member/MemberList.jsx
+++ frontend/src/page/member/MemberList.jsx
@@ -2,6 +2,7 @@ import { Box, Spinner, Table } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { MyHeading } from "../../components/root/MyHeading.jsx";
 
 export function MemberList() {
   const [memberList, setMemberList] = useState([]);
@@ -22,14 +23,19 @@ export function MemberList() {
   }
 
   return (
-    <Box>
-      <h3>회원 목록</h3>
+    <Box
+      mx={"auto"}
+      w={{
+        md: "500px",
+      }}
+    >
+      <MyHeading>회원 목록</MyHeading>
 
       <Table.Root interactive>
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeader>ID</Table.ColumnHeader>
-            <Table.ColumnHeader>EMAIL</Table.ColumnHeader>s
+            <Table.ColumnHeader>EMAIL</Table.ColumnHeader>
             <Table.ColumnHeader>가입일시</Table.ColumnHeader>
           </Table.Row>
         </Table.Header>
@@ -40,8 +46,8 @@ export function MemberList() {
               key={member.id}
             >
               <Table.Cell>{member.id}</Table.Cell>
-              <Table.Cell>{member.inserted}</Table.Cell>
               <Table.Cell>{member.email}</Table.Cell>
+              <Table.Cell>{member.inserted}</Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
